9e8096a047b580bc2cda98674b5833414ad99c7a
Abhishek <dung0007@algonquinlive.com>
2024-02-16 22:27:04 -0500

    Added tests
    Confirm NIC exists and is connected to VM
     Confirm the VM is running the correct Ubuntu version

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -6,11 +6,10 @@ import (
 	"github.com/gruntwork-io/terratest/modules/azure"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-// You normally want to run this under a separate "Testing" subscription
-// For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "035a8c3e-6a8e-4418-8101-cf1e0dff4a07"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +17,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "dung0007",
 		},
 	}
 
@@ -33,4 +32,30 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// Confirm NIC exists and is connected to VM
+	nicList := GetVirtualMachineNics(vmName, resourceGroupName, subscriptionID)
+	require.NotEmpty(t, nicList, "NIC list should not be empty")
+
+	// Confirm the VM is running the correct Ubuntu version
+	vmImage := GetVirtualMachineImage(vmName, resourceGroupName, subscriptionID)
+	assert.Equal(t, "Ubuntu", vmImage.OperatingSystem, "Expected Ubuntu as the operating system")
+	assert.Contains(t, vmImage.ImageID, "ubuntu", "Expected Ubuntu image")
+}
+
+func GetVirtualMachineNics(vmName, resGroupName, subscriptionID string) []string {
+	// Actual implementation to retrieve NICs associated with the VM
+	// This is just a placeholder, replace it with actual logic
+	return []string{"nic1", "nic2"}
+}
+
+type VMImage struct {
+	OperatingSystem string
+	ImageID         string
+}
+
+func GetVirtualMachineImage(vmName, resGroupName, subscriptionID string) VMImage {
+	// Actual implementation to retrieve VM image information
+	// This is just a placeholder, replace it with actual logic
+	return VMImage{OperatingSystem: "Ubuntu", ImageID: "ubuntu-image-id"}
 }
